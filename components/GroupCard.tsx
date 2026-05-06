da2192f3bf3d4ebccd4a8b856fc96d7de8b0ca5f
Dario Hunjadi <dario.hunjadi@hotmail.com>
2026-05-06 17:05:27 +0200

    created GroupCard

---
+++ components/GroupCard.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+type GroupCardIconProps = {
+  size: number;
+  color: string;
+};
+
+type GroupCardProps = {
+  icon: (props: GroupCardIconProps) => React.ReactNode;
+  label: string;
+};
+
+const GroupCard = ({ icon, label }: GroupCardProps) => {
+  return (
+    <View className="relative w-full p-8 flex-col gap-4 bg-primaryDark rounded-xl overflow-hidden mb-4">
+      {icon({ size: 48, color: "white" })}
+      <Text className="text-white font-bold text-xl">{label}</Text>
+
+      <View className="absolute inset-y-0 right-12 flex justify-center items-end opacity-20">
+        {icon({ size: 72, color: "white" })}
+      </View>
+    </View>
+  );
+};
+
+export default GroupCard;
