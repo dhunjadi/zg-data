58378139e024a80c798f48cd231caef2275a90bd
Dario Hunjadi <dario.hunjadi@hotmail.com>
2026-05-06 17:14:27 +0200

    rendering group cards

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -1,17 +1,65 @@
-import { useTheme } from "@react-navigation/native";
+import GroupCard from "@/components/GroupCard";
+import {
+  Building2,
+  Gavel,
+  GraduationCap,
+  HeartPulse,
+  Landmark,
+  Microscope,
+  PersonStanding,
+  Road,
+  Sprout,
+  TreePine,
+  Wallet,
+} from "lucide-react-native";
 import React from "react";
-import { Text } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
-  const { colors } = useTheme();
+  const groups = [
+    { label: "Obrazovanje, kultura i sport", icon: GraduationCap },
+    { label: "Regije i gradovi", icon: Building2 },
+    { label: "Ekonomije i financije", icon: Wallet },
+    { label: "Javni sektor i ustanove", icon: Landmark },
+    { label: "Okoliš", icon: TreePine },
+    { label: "Poljoprivreda, ribarstvo, šumarstvo i hrana", icon: Sprout },
+    {
+      label: "Pravosuđe, pravni sustav i javna sigurnost",
+      icon: Gavel,
+    },
+    { label: "Promet", icon: Road },
+    { label: "Stanovništvo i društvo", icon: PersonStanding },
+    { label: "Zdravstvo", icon: HeartPulse },
+    { label: "Znanost i tehnologija", icon: Microscope },
+  ];
 
   return (
-    <SafeAreaView
-      style={{ backgroundColor: colors.background }}
-      className="flex-1 p-4"
-    >
-      <Text>Home</Text>
+    <SafeAreaView className="flex-1 px-4 bg-neutral-100">
+      <View>
+        <FlatList
+          data={groups}
+          keyExtractor={(item) => item.label}
+          renderItem={({ item }) => (
+            <GroupCard
+              label={item.label}
+              icon={(props) => <item.icon {...props} />}
+            />
+          )}
+          ListHeaderComponent={
+            <>
+              <Text className="text-3xl text-primaryDark font-bold mb-2">
+                Otvoreni podaci Grada Zagreba
+              </Text>
+
+              <Text className="text-md text-neutral-700 mb-8">
+                Pristupite otvorenim bazama podataka za transparentnije i
+                pametnije upravljanje gradom.
+              </Text>
+            </>
+          }
+        />
+      </View>
     </SafeAreaView>
   );
 };
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,3 @@
-import { useColorScheme } from "@/hooks/use-color-scheme";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
@@ -11,44 +5,14 @@ import "react-native-reanimated";
 import "../global.css";
 
 const RootLayout = () => {
-  const colorScheme = useColorScheme();
-
-  const customLightTheme = {
-    ...DefaultTheme,
-    colors: {
-      ...DefaultTheme.colors,
-      primary: "#6200ee",
-      background: "#f9f9f9",
-      card: "#dadada",
-      text: "#333",
-      border: "#e0e0e0",
-      notification: "#ff453a",
-    },
-  };
-
-  const customDarkTheme = {
-    ...DarkTheme,
-    colors: {
-      ...DarkTheme.colors,
-      primary: "#bb86fc",
-      background: "#333",
-      card: "#1f1f1f",
-      text: "#f9f9f9",
-      border: "#272727",
-      notification: "#ff453a",
-    },
-  };
-
   return (
-    <ThemeProvider
-      value={colorScheme === "dark" ? customDarkTheme : customLightTheme}
-    >
+    <>
       <StatusBar style="auto" />
       <Stack>
         <Stack.Screen name="(protected)" options={{ headerShown: false }} />
         <Stack.Screen name="login" options={{ headerShown: false }} />
       </Stack>
-    </ThemeProvider>
+    </>
   );
 };
 
